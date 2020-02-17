68da0419226331e40f03f8866d0a53970a67db8a
Marc Rasell <marc.rasell@students.plymouth.ac.uk>
2020-02-17 09:52:13 +0000

    Update database.php

--- htdocs/database_classes/database.php
+++ htdocs/database_classes/database.php
@@ -57,5 +57,58 @@ class Database
 
         return $bookingID;
     }
+	
+	# will return a customerId of 0 if not found, returns the hashes password and customerId for an email address
+    public function customerLogin($email)
+    {
+        $connection = $this->getConnection();
+
+        # call customer login procedure
+        $sql = "CALL pr_customerLogin (:email,@hashedPassword,@customerId)";
+        $statement = $connection->prepare($sql);
+        $statement->bindValue(':email',$email);
+        $statement->execute();
+
+        # get output from procedure
+        $sql = "SELECT @hashedPassword,@customerId";
+        $statement = $connection->query($sql);
+        $row = $statement->fetch (PDO::FETCH_NUM);
+        $hashedPassword = $row[0];
+        $customerId = $row[1];
+
+        $statement = null;
+        $getConnection = null;
+
+        return array($hashedPassword,$customerId);
+    }
+
+    # register a new customer, if already registered returns $results = 'already registered'
+    public function registerCustomer($email,$firstName,$lastName,$pass1,$address,$postcode)
+    {
+        $connection = $this->getConnection();
+
+        # call register customer procedure
+        $sql = "CALL pr_registerCustomer (:email,:firstName,:lastName,:hashedPassword,:address,:postcode,@results)";
+        $statement = $connection->prepare($sql);
+        $hashedPassword = password_hash($pass1, PASSWORD_DEFAULT);
+        $statement->bindValue(':email',$email);
+        $statement->bindValue(':firstName',$firstName);
+        $statement->bindValue(':lastName',$lastName);
+        $statement->bindValue(':hashedPassword',$hashedPassword);
+		$statement->bindValue(':address',$address);
+		$statement->bindValue(':postcode',$postcode);
+        $statement->execute();
+
+        # get output from procedure
+        $sql = "SELECT @results";
+        $statement = $connection->query($sql);
+        $row = $statement->fetch(PDO::FETCH_NUM);
+        $results = $row[0];
+
+        $statement = null;
+        $getConnection = null;
+
+        return $results;
+    }
 
 }
