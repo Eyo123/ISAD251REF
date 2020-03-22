db908f6bd23dce8ea7b89038d918718e4dbb1345
Marc Rasell <marc.rasell@students.plymouth.ac.uk>
2020-03-22 15:35:07 +0000

    Update delete.php

--- htdocs/delete.php
+++ htdocs/delete.php
@@ -11,25 +11,52 @@ if (!isset($_SESSION['customerId']))
 {
     load();
 }
-print '<td><a href="delete.php?id=" class="btn btn-info">Delete your account</a></td></tr></tbody>';
+
+if($_SERVER['REQUEST_METHOD']=='POST')
+{
+	$confirm = secure_input($_POST['confirm']);
+	
+	if(empty($confirm))
+	{
+		$errors[] = 'Enter the confirmation word.';
+	}
+	
+	if($confirm != 'DELETE')
+	{
+		$errors[] = 'The confirmation word was incorrect.';
+	}
+	
+	if(empty($errors))
+	{
+		$customerID = $_SESSION['customerId'];
+		$database = new Database();
+		$database->deleteCustomer($customerId);
+
+		load();
+	}
+}
+
 ?>
 
+<section role="contentinfo" aria-label="Flightcrew delete account page">
+<div class="container">
+    <div class="row">
+        <h2 class="bg-dark text-white">Delete Account</h2>
+    </div>
+    <div class="row">
+        <form action="delete.php" method="post">
+            <div class="form-group">
+                <label for="confirm">Please enter the word DELETE to confirm account deletion:</label>
+                <input type="text" name="confirm" class="form-control" id="confirm" size="50"
+                       value="<?php if (isset($_POST['confirm'])) echo $_POST['confirm']; ?>" required>
+            </div>
+        </form>
+    </div>
+</div>
+</section>
 
 <?php
 
-    $customerID = $_SESSION['customerId'];
-    $database = new Database();
-    $database->deleteCustomer($customerId);
-
-    echo '<div class="container"><div class="row">';
-
-    echo '<h3 class="text-info">your account has been deleted</h3>';
-
-?>
-
-
-<?php
-
-
+include_once('includes/footer.html');
 
 ?>
