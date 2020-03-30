a287e4d6110a58718faacecdf17158ac4e43b5bc
Marc Rasell <marc.rasell@students.plymouth.ac.uk>
2020-03-30 11:48:28 +0100

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,5 +1,9 @@
 <?php
 
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(E_ALL);
+
 $page_title = "Flightcrew Home";
 
 require_once('database_classes/database.php');
